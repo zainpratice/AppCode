72256641b5cfa177b1ef419b29483438bc15f114
zainpratice <90721873+zainpratice@users.noreply.github.com>
2024-09-11 18:24:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    TODO LIST
+                    ZaYn Rauf TODO LIST
                 </Row>
   
                 <hr />
